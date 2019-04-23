0522680e3c4a63b095461ed7efe7160fdd574a27
sej <sej@needrent.dk>
2019-04-23 08:50:57 +0200

    JSON practice

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+let myLink = "http://kea-alt-del.dk/t5/api/productlist";
+
+function loadData(link){
+    fetch(link).then(e=>e.json()).then(data=>show(data));
+}
+
+function show(data){
+    data.forEach(object=>console.log(object));
+}
+
+loadData(myLink);
